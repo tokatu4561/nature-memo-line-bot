e9c19fbfb6499b1dd8a0e1502effe1955f45c329
tokatu4561 <ryobsk4561@gmail.com>
2022-12-30 00:58:08 +0900

    取り敢えずエアコンon offさせる

--- cmd/line-bot-task/main.go
+++ cmd/line-bot-task/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -32,16 +36,51 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 			case *linebot.TextMessage:
 				replyMessage := message.Text
-				_, err = line.Client.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-				if err != nil {
-					return events.APIGatewayProxyResponse{
-						Body:       err.Error(),
-						StatusCode: 500,
-					}, nil
+
+				switch replyMessage {
+				case "エアコン":
+					actions := []linebot.TemplateAction {
+						// linebot.NewPostbackAction("On", "appliances={air}&status=on", "on", "on"),
+						linebot.NewPostbackAction("On", "on", "on", ""),
+						linebot.NewPostbackAction("Off","off", "off", ""),
+					}
+					res := linebot.NewTemplateMessage(
+						"エアコンの電源を入れますか？",
+						linebot.NewButtonsTemplate("", "エアコンの電源を入れますか？", "please select", actions...),
+					)
+
+					_, err = line.Client.ReplyMessage(event.ReplyToken, res).Do()
+
+					log.Println(err)
+
+					if err != nil {
+						return events.APIGatewayProxyResponse{
+							Body:       err.Error(),
+							StatusCode: 500,
+						}, nil
+					}
+				case "照明":
+				case "テレビ":
 				}
+				break
 			default:
 			}
-		}
+		} else if event.Type == linebot.EventTypePostback {
+			postBackData := event.Postback.Data
+			log.Println("通過1")
+			switch postBackData {
+			case "on":
+				err = postRequest("エアコン", true)
+			case "off":
+				err = postRequest("エアコン", false)
+			}
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					Body:       err.Error(),
+					StatusCode: 500,
+				}, nil
+			}
+		} else {}
 	}
 
 	return events.APIGatewayProxyResponse{
@@ -68,6 +107,53 @@ func setUpLineClient() (*line.Line, error) {
 	line.Client = bot
 
 	return line, nil
+}
+
+func postRequest(appliances string, on bool) error {
+	var switchText string
+	if (on) {
+		switchText = "on"
+	} else {
+		switchText = "off"
+	}
+
+	// requestBody := struct {
+	// 	Button string `json:"button"`
+	// }{
+	// 	Button: switchText,
+	// }
+	
+    // jsonString, err := json.Marshal(requestBody)
+    // if err != nil {
+    //    return err
+    // }
+	values := url.Values{}
+    values.Set("button", switchText)
+	
+    endpoint := fmt.Sprintf("%s/%s", os.Getenv("API_URL"), "1/appliances/3ab1a2c4-a8c8-4fa4-b004-22b045d2b43c/light")
+	
+	log.Println(switchText)
+    req, err := http.NewRequest("POST", endpoint, strings.NewReader(values.Encode()))
+	if err != nil {
+        return err
+    }
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("API_TOKEN")))
+
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+        return err
+    }
+    defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+        log.Printf("http status code: %d", res.StatusCode)
+		log.Println(res.Body)
+    }
+
+	return nil
 }
 
 func main() {
